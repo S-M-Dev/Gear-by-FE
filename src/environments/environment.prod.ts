0d46d36cc35aad8bb72609238f94dd2b2e5ddeea
Pavel Razuvalau <pavel@razuvalov.com>
2021-10-24 20:56:06 +0300

    feat: setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  firebase: {
+    apiKey: 'AIzaSyC365OqBIxPcO4b0rTOyhkbwAAO2P9U28E',
+    authDomain: 'gear-by.firebaseapp.com',
+    projectId: 'gear-by',
+    storageBucket: 'gear-by.appspot.com',
+    messagingSenderId: '665972283173',
+    appId: '1:665972283173:web:d4ee7b02e080178ca66ccc',
+  },
 };
